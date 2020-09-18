60283cd691cde6d1eb1e405cc89492bce074a124
DiscoRiko <gilchikorel@technion.ac.il>
2020-09-18 17:37:33 +0300

    17.9.20 We finished extract_final_masks

--- hw3/CenterFaceMask.py
+++ hw3/CenterFaceMask.py
@@ -52,9 +52,9 @@ class CenterFaceMask(nn.Module):
             center_points.append(pic_center_points)
         return center_points
 
-    def extractLocalMasks(self, center_points, shape, size):
+    def extract_local_masks(self, center_points, shape, size):
         """
-        center_points = list of list of tuples - (x, y) - Nx10
+        center_points - list of list of tuples - (x, y) - Nx10
         shape - Nx(32*32)xHxW
         size - Nx2xHxW
         """
@@ -72,100 +72,68 @@ class CenterFaceMask(nn.Module):
                 resize_temp = T.Resize((pic_size[0, point[1], point[0]], pic_size[1, point[1], point[0]]))
                 mask = to_tensor(resize_temp(to_pil(shape_vec)))
                 print(f"mask shape:{mask.shape}")
-                pic_local_masks.update({'center_point_'+organ: point, 'mask_'+organ: mask})
+                pic_local_masks.update({organ: (point, mask.view((mask.shape[1], mask.shape[2])))})
             local_masks.append(pic_local_masks)
         return local_masks
 
-    def extractFinalMask(self, saliency, local_masks):
+    def extract_final_mask(self, saliency, local_masks):
+        """
+        saliency - Nx1xHxW
+        local_masks - list of N local masks dictionaries
+        """
         final_masks = []
-        for center_point, local_mask in local_masks:
+        pic_num = 0
+        for local_masks_dict in local_masks:
+            pic_final_masks = dict()
+            for organ, data in local_masks_dict.items():
+                center_point, local_mask = data
+                # Getting the size of the mask
+                h = local_mask.shape[0]
+                w = local_mask.shape[1]
 
-            # Getting the size of the mask
-            h = local_mask.shape[0]
-            w = local_mask.shape[1]
+                # Getting the global mask
+                global_row_1 = center_point[1] - (h // 2)
+                global_row_2 = global_row_1 + h
+                global_col_1 = center_point[0] - (w // 2)
+                global_col_2 = global_col_1 + w
 
-            # Getting the global mask
-            row_1 = center_point[0] - (h // 2)
-            row_2 = row_1 + h - 1
-            col_1 = center_point[1] - (w // 2)
-            col_2 = col_1 + w - 1
+                local_diff_row_1 = 0
+                local_diff_row_2 = h
+                local_diff_col_1 = 0
+                local_diff_col_2 = w
 
-            diff_row_1 = 0
-            diff_row_2 = h + 1
-            diff_col_1 = 0
-            diff_col_2 = w + 1
+                if global_row_1 < 0:
+                    local_diff_row_1 = (0 - global_row_1)
+                    global_row_1 = 0
 
-            if (row_1 < 0):
-                diff_row_1 = (0 - row_1)
-                row_1 = 0
+                if global_row_2 > 512:
+                    local_diff_row_2 = h - (global_row_2 - 512)
+                    global_row_2 = 512
 
-            if (row_2 > 511):
-                diff_row_2 = h - (row_2 - 511)
-                row_2 = 511
+                if global_col_1 < 0:
+                    local_diff_col_1 = (0 - global_col_1)
+                    global_col_1 = 0
 
-            if (col_1 < 0):
-                diff_col_1 = (0 - col_1)
-                col_1 = 0
+                if global_col_2 > 512:
+                    local_diff_col_2 = w - (global_col_2 - 512)
+                    global_col_1 = 512
 
-            if (col_2 > 511):
-                diff_col_2 = w - (col_2 - 511)
-                col_1 = 511
+                global_mask = saliency[pic_num, 0, global_row_1:global_row_2, global_col_1:global_col_2]
 
-            global_mask = saliency[0, 0, row_1:(row_2 + 1), col_1:(col_2 + 1)]
+                global_mask = global_mask.reshape(global_row_2 - global_row_1, global_col_2 - global_col_1)
+                cropped_local_mask = local_mask[local_diff_row_1:local_diff_row_2, local_diff_col_1:local_diff_col_2]
+                # Sigmoid as said in the article
+                sig = torch.nn.Sigmoid()
+                cropped_local_mask = sig(cropped_local_mask)
+                cropped_global_mask = sig(global_mask)
 
-            global_mask = global_mask.reshape(row_2 - row_1 + 1, col_2 - col_1 + 1)
-            cropped_local_mask = local_mask[diff_row_1:diff_row_2, diff_col_1:diff_col_2]
+                # Getting the final mask
+                final_mask = cropped_local_mask * cropped_global_mask
+                pic_final_masks.update({organ: (center_point, final_mask)})
+            pic_num += 1
+            final_masks.append(pic_final_masks)
 
-            sig = torch.nn.Sigmoid()
-            cropped_local_mask = sig(cropped_local_mask)
-
-            # Getting the final mask
-            final_mask = cropped_local_mask * global_mask
-            final_masks.append(final_mask)
         return final_masks
-
-
-def checking(saliency, center_point, mask):
-        # Getting the size of the mask
-        h = mask.shape[0]
-        w = mask.shape[1]
-        # Getting the global mask
-        row_1 = center_point[0] - (h//2)
-        row_2 = row_1 + h - 1
-        col_1 = center_point[1] - (w // 2)
-        col_2 = col_1 + w - 1
-
-        diff_row_1 = 0
-        diff_row_2 = h + 1
-        diff_col_1 = 0
-        diff_col_2 = w + 1
-
-        if (row_1 < 0):
-            diff_row_1 = (0 - row_1)
-            row_1 = 0
-        if (row_2 > 7):
-            diff_row_2 = h - (row_2 - 7)
-            row_2 = 7
-        if (col_1 < 0):
-            diff_col_1 = (0 - col_1)
-            col_1 = 0
-        if (col_2 > 7):
-            diff_col_2 = w - (col_2 - 7)
-            col_2 = 7
-        global_mask = saliency[0, 0, row_1:(row_2+1), col_1:(col_2+1)]
-
-        print(f"global mask shape: {global_mask.shape}")
-        print(f"global mask: {global_mask}")
-        global_mask = global_mask.reshape(row_2 - row_1 + 1, col_2 - col_1 + 1)
-        cropped_mask = mask[diff_row_1:diff_row_2, diff_col_1:diff_col_2]
-        print(f"cropped mask shape: {cropped_mask.shape}")
-        print(f"cropped mask: {cropped_mask}")
-        #sig = torch.nn.Sigmoid()
-        #cropped_mask = sig(cropped_mask)
-        # Getting the final mask
-
-        final_mask = cropped_mask * global_mask
-        return final_mask
 
 
 def loss_size (all_sizes, center_points, actual_sizes):
@@ -188,3 +156,45 @@ def loss_size (all_sizes, center_points, actual_sizes):
     actual_sizes_list = torch.stack(actual_sizes_list, dim=1)
 
     return loss(sizes_list, actual_sizes_list)/sizes_list.shape[1]
+
+#def checking(saliency, center_point, mask):
+#        # Getting the size of the mask
+#        h = mask.shape[0]
+#        w = mask.shape[1]
+#        # Getting the global mask
+#        row_1 = center_point[0] - (h//2)
+#        row_2 = row_1 + h - 1
+#        col_1 = center_point[1] - (w // 2)
+#        col_2 = col_1 + w - 1
+#
+#        diff_row_1 = 0
+#        diff_row_2 = h + 1
+#        local_diff_col_1 = 0
+#        diff_col_2 = w + 1
+#
+#        if (row_1 < 0):
+#            diff_row_1 = (0 - row_1)
+#            row_1 = 0
+#        if (row_2 > 7):
+#            diff_row_2 = h - (row_2 - 7)
+#            row_2 = 7
+#        if (col_1 < 0):
+#            local_diff_col_1 = (0 - col_1)
+#            col_1 = 0
+#        if (col_2 > 7):
+#            diff_col_2 = w - (col_2 - 7)
+#            col_2 = 7
+#        global_mask = saliency[0, 0, row_1:(row_2+1), col_1:(col_2+1)]
+#
+#        print(f"global mask shape: {global_mask.shape}")
+#        print(f"global mask: {global_mask}")
+#        global_mask = global_mask.reshape(row_2 - row_1 + 1, col_2 - col_1 + 1)
+#        cropped_mask = mask[diff_row_1:diff_row_2, local_diff_col_1:diff_col_2]
+#        print(f"cropped mask shape: {cropped_mask.shape}")
+#        print(f"cropped mask: {cropped_mask}")
+#        #sig = torch.nn.Sigmoid()
+#        #cropped_mask = sig(cropped_mask)
+#        # Getting the final mask
+#
+#        final_mask = cropped_mask * global_mask
+#        return final_mask
